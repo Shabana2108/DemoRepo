b16eb6bff65fb4dd7365f88ba55a73965fa31e5b
Shabana2108 <89011508+Shabana2108@users.noreply.github.com>
2024-08-01 07:47:40 +0530

    updated variable

--- src/main/java/pageObjects/HelpDesk.java
+++ src/main/java/pageObjects/HelpDesk.java
@@ -31,7 +31,7 @@ public class HelpDesk {
     WebDriver driver = new ChromeDriver();
     Select sel;
     JavascriptExecutor js;
-    WebDriverWait wb;
+    WebDriverWait WB;
     Robot robot;
     	
    
